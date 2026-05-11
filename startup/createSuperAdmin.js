62ed88b318bfca39f451440615a6f18fe86e87ca
Seif El_Sayed <seifsayed121@gmail.com>
2026-05-11 18:07:45 +0300

    create super admin

---
+++ startup/createSuperAdmin.js
@@ -0,0 +1,24 @@
+const prisma = require("./db");
+require("colors");
+const asyncHandler = require("express-async-handler");
+const { SUPER_ADMIN } = require("../utils/constants");
+
+const createSuperAdmin = asyncHandler(async () => {
+  const admin = await prisma.admin.findFirst({where: {isSuperAdmin: true }})
+  if (!admin) {
+    const admin = await prisma.admin.create({
+      data: {
+        firstName: "Super",
+        lastName: "Admin",
+        role: SUPER_ADMIN,
+        email: process.env.SUPER_ADMIN_EMAIL,
+        password: process.env.SUPER_ADMIN_PASSWORD,
+        isVerified: true,
+        isSuperAdmin: true,
+      } 
+    })
+    console.log("=== Super Admin created successfully ===".green);
+  }
+})
+
+module.exports = createSuperAdmin;
