d22b12c246e18c8dbd87ffc412ffa4d717d3e859
Seif El_Sayed <seifsayed121@gmail.com>
2026-05-14 23:55:21 +0300

    feat: add doctor

---
+++ controllers/doctor.controller.js
@@ -0,0 +1,53 @@
+const asyncHandler = require("express-async-handler")
+const prisma = require("../startup/db")
+const ApiFeatures = require("../utils/ApiFeatures")
+const ApiError = require("../utils/ApiError")
+const { translate } = require("../utils/translation")
+const {getAvailableSlots} = require("../utils/getAvaliabeSlots")
+
+const dayNames = {
+    0: "Sunday",
+    1: "Monday",
+    2: "Tuesday",
+    3: "Wednesday",
+    4: "Thursday",
+    5: "Friday",
+    6: "Saturday"
+}
+class DoctorController {
+
+    // @desc add doctor
+    // @route POST /doctors
+    // @access private
+    addDoctor = asyncHandler(async (req, res, next) => {
+        const { email, workingHours, ...doctorData } = req.body
+        
+        const existDoctor = await prisma.doctor.findUnique({
+            where: { email }
+        })
+        if (existDoctor) return next(new ApiError("Doctor already exist", 400))
+
+        const doctor = await prisma.doctor.create({
+            data: {
+                ...doctorData,
+                email,
+                workingHours: {
+                    create: workingHours
+                }
+            },
+            include: {
+                workingHours: true
+            }
+        })
+
+        res.status(201).json({
+            success: true,
+            message: "Doctor created successfully",
+            data: doctor
+        })
+    })
+
+   
+}
+
+module.exports = new DoctorController()
