1ace78f5fcf72d2273803828a49e51ff154a451e
Seif El_Sayed <seifsayed121@gmail.com>
2026-05-18 01:16:46 +0300

    doctor routes

---
+++ routes/doctor.route.js
@@ -0,0 +1,80 @@
+const express = require("express");
+// Classes 
+const DoctorController = require("../controllers/doctor.controller");
+const DoctorValidator = require("../validators/doctor.validator");
+const WorkingHoursController = require("../controllers/workingHours.controller");
+const WorkingHoursValidator = require("../validators/workingHours.validator");
+//middlewares
+const { protect, allowedTo } = require("../middlewares/auth.middleware");
+const { ADMIN, SUPER_ADMIN } = require("../utils/constants");
+// Router
+const router = express.Router();
+
+router.route("/")
+    .post(
+        protect, 
+        allowedTo(SUPER_ADMIN, ADMIN), 
+        DoctorValidator.validateAddDoctor, 
+        DoctorController.addDoctor
+    )
+    .get(
+        DoctorController.getAllDoctors
+    )
+
+router.route("/:id")
+    .get(
+        DoctorController.getOneDoctor
+    )
+    .post(
+        protect, 
+        allowedTo(SUPER_ADMIN, ADMIN), 
+        WorkingHoursValidator.validateAddWorkingHours,
+        WorkingHoursController.addWorkingHours
+    )
+    .patch(
+        protect,
+        allowedTo(SUPER_ADMIN, ADMIN),
+        DoctorValidator.validateUpdateDoctor,
+        DoctorController.updateDoctor
+    )
+
+router.route("/:id/delete")
+    .patch(
+        protect,
+        allowedTo(SUPER_ADMIN, ADMIN), 
+        DoctorController.toggleDeleteDoctor
+    )
+
+router.route("/:id/leaves")
+    .patch(
+        protect,
+        allowedTo(SUPER_ADMIN, ADMIN),
+        WorkingHoursController.addDoctorLeave
+    )
+
+router.route("/:id/leaves-cancel")
+    .patch(
+        protect,
+        allowedTo(SUPER_ADMIN, ADMIN),
+        WorkingHoursController.editDoctorLeave
+    )
+
+router.route("/:id/available-slots")
+    .get(
+        DoctorController.getAvailableSlots
+    )
+
+router.route("/:id/working-hours/:whId")
+    .patch(
+        protect, 
+        allowedTo(SUPER_ADMIN, ADMIN), 
+        WorkingHoursValidator.validateUpdateWorkingHours, 
+        WorkingHoursController.updateWorkingHours
+    )
+    .delete(
+        protect, 
+        allowedTo(SUPER_ADMIN, ADMIN), 
+        WorkingHoursController.deleteWorkingHours
+    )
+
+module.exports = router;
